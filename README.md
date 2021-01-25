a0d3cef5a6c177f2fc558789f4a026789ad7fd8f
Yamid Montero Lozano <73442549+Y4m1D@users.noreply.github.com>
2021-01-25 01:11:15 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# ProgrammingAssignment2-master
+ Actividad Semana 2 Coursera RProgramming
---
+++ cachematrix.R
@@ -0,0 +1,35 @@
+## it exercise is focused in solve the activity 2 week 3 R Programming on Coursera
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+
+
+makeCacheMatrix <- function(x = matrix(sample(1:100,9),3,3)) {
+  s <- NULL
+  set <- function(y) {
+    x <<- y
+    s <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) s <<- solve
+  getsolve <- function() s
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
+}
+
+
+## this function Returns a matrix that is the inverse of 'x'
+
+cacheSolve <- function(x, ...) {
+  s <- x$getsolve()
+  if(!is.null(s)) {
+    message("getting inversed matrix")
+    return(s)
+  }
+  data <- x$get()
+  s <- solve(data, ...)
+  x$setsolve(s)
+  s
+}
+cacheSolve(my_matrix) ## testink the function cacheSolve
+
+        
